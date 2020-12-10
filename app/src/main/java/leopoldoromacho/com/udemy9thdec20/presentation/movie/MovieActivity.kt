1cef42dfd74476d3bc9eb27e1935674c2fabeaba
Leopoldo Romacho <leopoldo.romacho@bestsecret.com>
2020-12-10 13:29:30 +0100

    Data binding added to MovieActivity

--- app/src/main/java/leopoldoromacho/com/udemy9thdec20/presentation/movie/MovieActivity.kt
+++ app/src/main/java/leopoldoromacho/com/udemy9thdec20/presentation/movie/MovieActivity.kt
@@ -3,19 +3,38 @@ package leopoldoromacho.com.udemy9thdec20.presentation.movie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import leopoldoromacho.com.udemy9thdec20.R
 import leopoldoromacho.com.udemy9thdec20.databinding.ActivityHomeBinding
 import leopoldoromacho.com.udemy9thdec20.databinding.ActivityMovieBinding
+import leopoldoromacho.com.udemy9thdec20.presentation.di.core.Injector
+import leopoldoromacho.com.udemy9thdec20.presentation.movie.MovieViewModel
+import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMovieBinding
+    @Inject
+    lateinit var factory: MovieViewModelFactory
+
+    private lateinit var movieViewModel: MovieViewModel
+    private lateinit var binding:ActivityMovieBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_movie)
+        (application as Injector).createMovieSubComponent()
+            .inject(this)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
+        movieViewModel=ViewModelProvider(this,factory)
+            .get(MovieViewModel::class.java)
+        val responseLiveData = movieViewModel.getMovies()
+        responseLiveData.observe(this, Observer {
+            Log.i("MYTAG", it.toString())
+        })
+
     }
-
 }
