a850074d35accb9e13f44fb8bb3fa6ba6d64aa88
Leopoldo Romacho <leopoldo.romacho@bestsecret.com>
2020-12-10 10:10:59 +0100

    Presentation layer added

--- app/src/main/java/leopoldoromacho/com/udemy9thdec20/presentation/MainActivity.kt
+++ app/src/main/java/leopoldoromacho/com/udemy9thdec20/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package leopoldoromacho.com.udemy9thdec20
+package leopoldoromacho.com.udemy9thdec20.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import leopoldoromacho.com.udemy9thdec20.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
