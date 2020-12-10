257451d59fdc2825a42fe4cca541a60472599497
Leopoldo Romacho <leopoldo.romacho@bestsecret.com>
2020-12-10 10:51:25 +0100

    Basic DataBinding working fine

--- app/src/main/java/leopoldoromacho/com/udemy9thdec20/presentation/HomeActivity.kt
+++ app/src/main/java/leopoldoromacho/com/udemy9thdec20/presentation/HomeActivity.kt
@@ -1,10 +1,13 @@
 package leopoldoromacho.com.udemy9thdec20.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import leopoldoromacho.com.udemy9thdec20.R
 import leopoldoromacho.com.udemy9thdec20.databinding.ActivityHomeBinding
+import leopoldoromacho.com.udemy9thdec20.presentation.movie.MovieActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -13,5 +16,37 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        setUpButtonActionForMovies()
+
+        setUpButtonActionForArtists()
+
+        setUpButtonActionForTvShows()
+    }
+
+
+    private fun setUpButtonActionForMovies() {
+        binding.movieButton.setOnClickListener {
+            val intent = Intent(this, MovieActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setUpButtonActionForArtists() {
+        binding.tvButton.setOnClickListener {
+            val text = "TvShows ->Stay tuned - coming next!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        }
+    }
+
+    private fun setUpButtonActionForTvShows() {
+        binding.artistsButton.setOnClickListener {
+            val text = "Artists -> Stay tuned - coming next!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        }
     }
 }
--- app/src/main/java/leopoldoromacho/com/udemy9thdec20/presentation/movie/MovieActivity.kt
+++ app/src/main/java/leopoldoromacho/com/udemy9thdec20/presentation/movie/MovieActivity.kt
@@ -1,18 +1,21 @@
 package leopoldoromacho.com.udemy9thdec20.presentation.movie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import leopoldoromacho.com.udemy9thdec20.R
 import leopoldoromacho.com.udemy9thdec20.databinding.ActivityHomeBinding
+import leopoldoromacho.com.udemy9thdec20.databinding.ActivityMovieBinding
 
 class MovieActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityMovieBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
     }
+
 }
